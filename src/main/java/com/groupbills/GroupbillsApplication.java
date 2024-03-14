f12009dbd1f32ee1d1879b5b7bbfe8daffeaaf81
hlmg <devhlmg@gmail.com>
2024-03-14 15:48:22 +0900

    initial commit

---
+++ src/main/java/com/groupbills/GroupbillsApplication.java
@@ -0,0 +1,13 @@
+package com.groupbills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupbillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupbillsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/groupbills/GroupbillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupbills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupbillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
